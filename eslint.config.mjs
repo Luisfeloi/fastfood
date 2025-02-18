ec8e9f4115f666a9ed0bb56f9c3518bf98db8d7e
Luis Cunha <lfemcunha@hotmail.com>
2025-02-18 14:24:31 -0300

    chore: add eslint sort and prettier tailwind plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import { simpleImportSort } from "eslint-config-simple-import-sort"; /*11.2 (gzipped: 3.9K)*/
 import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
